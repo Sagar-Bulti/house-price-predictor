64f16b65b34a51e693f0e12484714eae8255ad50
Sagar <sagarbultibulti@gmail.com>
2026-04-25 19:53:27 +0530

    Change app to run on host and port from env
    Updated app to run on host 0.0.0.0 and specified port from environment variable.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 from param import output
+import os
 
 app = Flask(__name__)
 
@@ -87,4 +88,4 @@ def predict():
         )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
